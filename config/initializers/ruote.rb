cac228a436e3dc4229c884c5a227bd59a8f83cca
U-LUXRESEARCHINC\chris.greene <chris.greene@lux-bos-e6320-6.luxresearchinc.com>
2012-01-12 12:30:05 -0500

    Giving in - Hash Storage only

--- config/initializers/ruote.rb
+++ config/initializers/ruote.rb
@@ -1,18 +1,18 @@
 
-#require 'ruote/storage/hash_storage'
-require 'ruote/storage/fs_storage'
+require 'ruote/storage/hash_storage'
+#require 'ruote/storage/fs_storage'
 
 
 #
 # initialize engine
 
-#RuoteKit.engine = Ruote::Engine.new(
-  #Ruote::Worker.new(
-    #Ruote::HashStorage.new()))
-    
 RuoteKit.engine = Ruote::Engine.new(
   Ruote::Worker.new(
-    Ruote::FileStorage.new()))
+    Ruote::HashStorage.new()))
+    
+#RuoteKit.engine = Ruote::Engine.new(
+  #Ruote::Worker.new(
+    #Ruote::FileStorage.new()))
 
 #
 # register participants
